f2a1872503d3071ae6e2f5410e74bee65e702431
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-16 21:07:09 +0200

    Create juz.dart

---
+++ lib/models/juz.dart
@@ -0,0 +1,17 @@
+class Juz {
+  final int number;
+  final String name;
+  final int startSurah;
+  final int startAyah;
+  final int endSurah;
+  final int endAyah;
+
+  const Juz({
+    required this.number,
+    required this.name,
+    required this.startSurah,
+    required this.startAyah,
+    required this.endSurah,
+    required this.endAyah,
+  });
+}

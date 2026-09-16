1ffbb003cf4851969edee1e4a6ad4a5761ce3b10
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-16 21:04:57 +0200

    Create app_routes.dart

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,8 @@
+abstract final class AppRoutes {
+  static const String splash = '/';
+  static const String home = '/home';
+  static const String quran = '/quran';
+  static const String recitation = '/recitation';
+  static const String memorization = '/memorization';
+  static const String settings = '/settings';
+}

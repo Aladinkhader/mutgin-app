bf2c79586bfe3b7301efb09bd21df4242a2210c0
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-16 21:06:42 +0200

    Create ayah.dart

---
+++ lib/models/ayah.dart
@@ -0,0 +1,13 @@
+class Ayah {
+  final int number;
+  final int surahNumber;
+  final int ayahNumber;
+  final String text;
+
+  const Ayah({
+    required this.number,
+    required this.surahNumber,
+    required this.ayahNumber,
+    required this.text,
+  });
+}

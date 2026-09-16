f8138a5dda6084f45a9ce2e5a3338fe8d8a8e7ed
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-16 21:01:53 +0200

    Create app_radius.dart

---
+++ lib/core/theme/app_radius.dart
@@ -0,0 +1,13 @@
+abstract final class AppRadius {
+  static const double xs = 6;
+  static const double sm = 10;
+  static const double md = 14;
+  static const double lg = 18;
+  static const double xl = 24;
+  static const double xxl = 32;
+
+  static const double button = 16;
+  static const double card = 20;
+  static const double dialog = 24;
+  static const double bottomSheet = 28;
+}

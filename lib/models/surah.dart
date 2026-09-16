cee94326c27bf1a6974a70e4d71e05fa45e3cb49
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-16 21:06:13 +0200

    Create surah.dart

---
+++ lib/models/surah.dart
@@ -0,0 +1,15 @@
+class Surah {
+  final int number;
+  final String name;
+  final String arabicName;
+  final int numberOfAyahs;
+  final String revelationType;
+
+  const Surah({
+    required this.number,
+    required this.name,
+    required this.arabicName,
+    required this.numberOfAyahs,
+    required this.revelationType,
+  });
+}

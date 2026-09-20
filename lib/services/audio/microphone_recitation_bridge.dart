0f349fc5e47177e6991a237d03b6335657ef1082
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-20 14:24:44 +0200

    Create microphone_recitation_bridge.dart

---
+++ lib/services/audio/microphone_recitation_bridge.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'microphone_audio_service.dart';
+
+class MicrophoneRecitationBridge {
+  final MicrophoneAudioService microphone;
+
+  StreamSubscription<List<int>>? _subscription;
+
+  MicrophoneRecitationBridge({
+    MicrophoneAudioService? microphone,
+  }) : microphone = microphone ?? MicrophoneAudioService();
+
+  bool get isListening => microphone.isListening;
+
+  Stream<List<int>> get audioStream => microphone.audioStream;
+
+  Future<bool> requestPermission() {
+    return microphone.requestPermission();
+  }
+
+  Future<void> start() async {
+    await microphone.startListening();
+  }
+
+  Future<void> stop() async {
+    await microphone.stopListening();
+  }
+
+  void listen(
+    void Function(List<int> audioData) onAudio,
+  ) {
+    _subscription?.cancel();
+
+    _subscription = microphone.audioStream.listen(onAudio);
+  }
+
+  Future<void> cancelListening() async {
+    await _subscription?.cancel();
+    _subscription = null;
+  }
+
+  Future<void> dispose() async {
+    await cancelListening();
+    await microphone.dispose();
+  }
+}

41e3b1626500b70181ee95030285d151b5020c2f
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-20 13:18:29 +0200

    Create recitation_action_button.dart

---
+++ lib/features/recitation/widgets/recitation_action_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/theme/app_colors.dart';
+import '../../../core/theme/app_radius.dart';
+import '../../../core/theme/app_shadows.dart';
+import '../../../core/theme/app_spacing.dart';
+import '../../../core/theme/app_text_styles.dart';
+
+class RecitationActionButton extends StatelessWidget {
+  final bool isListening;
+  final VoidCallback? onPressed;
+
+  const RecitationActionButton({
+    super.key,
+    required this.isListening,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        width: 82,
+        height: 82,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isListening
+              ? AppColors.error
+              : AppColors.emerald,
+          boxShadow: isListening
+              ? AppShadows.goldGlow
+              : AppShadows.emeraldGlow,
+        ),
+        child: Icon(
+          isListening
+              ? Icons.stop_rounded
+              : Icons.mic_rounded,
+          color: AppColors.textPrimary,
+          size: 34,
+        ),
+      ),
+    );
+  }
+}
